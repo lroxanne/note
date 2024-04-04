501ca74c8492a66938598de12c9a1356ffd0586b
davidyh3 <yuhan59rz@gmail.com>
2024-04-04 03:17:07 -0400

    C:\Users\yuhan\AndroidStudioProjects\note\app\build\tmp\kapt3\stubs\debug\com\example\note1\data\database\NoteDatabase.java:10: �x��: Schema export directory was not provided to the annotation processor so Room cannot export the schema. You can either provide `room.schemaLocation` annotation processor argument by applying the Room Gradle plugin (id 'androidx.room') OR set exportSchema to false. public abstract class NoteDatabase extends androidx.room.RoomDatabase {

---
+++ app/src/main/java/com/example/note1/ui/AddEditNoteFragment.kt
@@ -0,0 +1,8 @@
+package com.example.note1.ui
+
+import androidx.fragment.app.Fragment
+import com.example.note1.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddEditNoteFragment: Fragment(R.layout.fragment_addeditnotes)
---
+++ app/src/main/java/com/example/note1/ui/NoteFragment.kt
@@ -0,0 +1,8 @@
+package com.example.note1.ui
+
+import androidx.fragment.app.Fragment
+import com.example.note1.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class NoteFragment: Fragment(R.layout.fragment_notes)
